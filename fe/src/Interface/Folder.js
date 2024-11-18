f1e8a71bc79ccf8f8cc15e456c3b00ba6bb9484a
BIMinf <mby7138@naver.com>
2024-11-18 16:53:23 +0900

    라우팅 실험 중

--- fe/src/Interface/Folder.js
+++ fe/src/Interface/Folder.js
@@ -4,11 +4,9 @@ import { useState } from 'react';
 
 
 export function Folder({ 
-  창닫기, 레퍼런스열기, 
-  에이딕트열기, 이핏열기,
-  할일앱열기, 게임2048열기,
-  뮤레카열기, 로테이션열기, 
-  그라데이션열기, 카운터열기 }) {
+  창닫기, 
+  레퍼런스열기, 
+  에이딕트열기 }) {
 
   const [state, setState] = useState({
     showDevelopment: true,
@@ -23,9 +21,6 @@ export function Folder({
     } else if (type === 'Document') {
       setState({ showDevelopment: false, showDocument: true, showDesign: false,  showPractice: false });
     } 
-    // else if (type === 'Design') {
-    //   setState({ showDevelopment: false, showDocument: false, showDesign: true,  showPractice: false });
-    // } 
     else if (type === 'Practice') {
       setState({ showDevelopment: false, showDocument: false, showDesign: false,  showPractice: true });
     }
@@ -38,7 +33,6 @@ export function Folder({
           <ul>
             <li className="Favorites" onClick={() => onClick('Development')}>📁 Development</li>
             <li className="Favorites" onClick={() => onClick('Document')}>📁 Document</li>
-            {/* <li className="Favorites" onClick={() => onClick('Design')}>📁 Design</li> */}
             <li className="Favorites" onClick={() => onClick('Practice')}>📁 Practice</li>
           </ul>
         </aside>
@@ -52,24 +46,6 @@ export function Folder({
             </div>
           )}
 
-          {state.showDevelopment && (
-            <div className="Icon" onClick={뮤레카열기}>
-              <div className="Upper">
-                <div className="Inner">Dev</div>
-              </div>
-              <div className="Lower">뮤레카</div>
-            </div>
-          )}
-
-          {state.showDevelopment && (
-            <div className="Icon" onClick={이핏열기}>
-              <div className="Upper">
-                <div className="Inner">Dev</div>
-              </div>
-              <div className="Lower">개발 중</div>
-            </div>
-          )}
-
           {state.showDocument && (
             <div className="Icon" onClick={레퍼런스열기}>
               <div className="Upper">
@@ -78,52 +54,6 @@ export function Folder({
               <div className="Lower">레퍼런스 사이트</div>
             </div>
           )}
-
-          {/* {state.showPractice && (
-            <div className="Icon" onClick={할일앱열기}>
-              <div className="Upper">
-                <div className="Inner">Dev</div>
-              </div>
-              <div className="Lower">투두리스트</div>
-            </div>
-          )} */}
-
-          {/* {state.showPractice && (
-            <div className="Icon" onClick={카운터열기}>
-              <div className="Upper">
-                <div className="Inner">Pratice</div>
-              </div>
-              <div className="Lower">카운터 연습</div>
-            </div>
-          )} */}
-
-          {state.showPractice && (
-            <div className="Icon" onClick={그라데이션열기}>
-              <div className="Upper">
-                <div className="Inner">Pratice</div>
-              </div>
-              <div className="Lower">캔버스 연습</div>
-            </div>
-          )}
-
-          {state.showPractice && (
-            <div className="Icon" onClick={로테이션열기}>
-              <div className="Upper">
-                <div className="Inner">Pratice</div>
-              </div>
-              <div className="Lower">슬라이더 연습</div>
-            </div>
-          )}
-
-          {/* {state.showPractice && (
-            <div className="Icon" onClick={게임2048열기}>
-              <div className="Upper">
-                <div className="Inner">Pratice</div>
-              </div>
-              <div className="Lower">게임2048</div>
-            </div>
-          )} */}
-          
         </section>
       </div>
     </Window>
