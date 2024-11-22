81e1a39476c28a4a8763b260f4d93511cd6d8e52
BIMinf <mby7138@naver.com>
2024-11-22 17:59:32 +0900

    20241122

--- fe/src/Browser/Glereka/Component/Glereka_Header.js
+++ fe/src/Browser/Glereka/Component/Glereka_Header.js
@@ -6,7 +6,7 @@ export function GlerekaHeader() {
   return (
     <header>
       <Link to="/Portfolio/glereka/">Pharagraph</Link>
-      <Link to="/Portfolio/glereka/write">작성</Link>
+      <Link to="/Portfolio/glereka/posting">작성</Link>
       <Link to="/Portfolio/glereka/login">로그인</Link>
       <Link to="/Portfolio/glereka/signup">회원가입</Link>
       <Link to="/Portfolio/glereka/profile">프로필</Link>
