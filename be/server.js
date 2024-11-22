24cd4ca86abd7011f9e8e0b344264e7e636f7b09
BIMinf <mby7138@naver.com>
2024-11-22 17:59:55 +0900

    20241122

--- be/server.js
+++ be/server.js
@@ -1,9 +1,11 @@
-require('dotenv').config();  // 환경 변수 로드
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) 
 
 const { MongoClient } = require("mongodb");
 
@@ -13,17 +15,18 @@ const url = process.env.MONGODB_URI;
 new MongoClient(url)
   .connect()
   .then((client) => {
-    console.log("DB가 연결되었습니다.");
+    console.log("데이터베이스가 MongoDB에 연결되었습니다.");
     db = client.db("Board");
     app.listen(process.env.PORT || 8080, () => {
-      console.log(`서버가 localhost:${process.env.PORT || 8080}에서 열렸습니다.`);
+      console.log(`서버가 localhost:${process.env.PORT || 8080}에 연결되었습니다.`);
     });
   })
   .catch((err) => {
     console.log(err);
   });
 
-app.get("/Portfolio/community/list", async (요청, 응답) => {
-  let result = await db.collection("Post").find().toArray();
-  응답.send(result);
+
+app.post('/glereka/posting', async (요청, 응답) => {
+  await db.collection('Post').insertOne(요청.body)
+  console.log(요청.body)
 });
